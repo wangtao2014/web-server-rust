7a619177843fe5f5f6f4c68ca6fda7b5ead3bd98
wangtao <wangtao20100715@gmail.com>
2022-12-26 22:10:19 +0800

    create tcp server

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Respnse from server: {:?}",
+        str::from_utf8(&buffer).unwrap()
+    );
 }
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{ Read, Write };
 use std::net::TcpListener;
 
 fn main() {
@@ -5,7 +6,11 @@ fn main() {
     println!("Running on port 3000...");
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let mut stream = stream.unwrap();
         println!("Connection established!");
+
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
     }
 }
